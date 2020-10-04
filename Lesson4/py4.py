d46f5fdfe915625911aa950059c37b43bf5b0fed
MaksimMarynau <oliverknightbel@gmail.com>
2020-10-04 13:21:50 +0200

    release v1

--- Lesson4/py4.py
+++ Lesson4/py4.py
@@ -1,3 +1,15 @@
+"""
+ZADANIE 5.1
+Stworzyć plik rekurencja.py i zapisać w nim funkcje z zadań 4.3 (factorial), 4.4 (fibonacci). Sprawdzić operacje importu
+i przeładowania modułu.
+"""
 import rekurencja
-print(rekurencja.factorial(6))
-print(rekurencja.fibonacci(5))
+print(rekurencja.factorial(5))
+print(rekurencja.fibonacci(10))
+
+"""
+ZADANIE 5.2
+Stworzyć plik fracs.py i zapisać w nim funkcje do działań na ułamkach. Ułamek będzie reprezentowany przez listę
+dwóch liczb całkowitych [licznik, mianownik]. Napisać kod testujący moduł fracs. Nie należy korzystać z klasy
+Fraction z modułu fractions. Można wykorzystać funkcję fractions.gcd() implementującą algorytm Euklidesa.
+"""
